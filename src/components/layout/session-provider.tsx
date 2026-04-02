76a585c7cfa6c58da13a9984c9b9a6de4281012a
Virendra Vikram Singh <153907622+virendra0817@users.noreply.github.com>
2026-04-02 11:24:14 +0530

    Remove session prop from SessionProvider

--- src/components/layout/session-provider.tsx
+++ src/components/layout/session-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
 
-export function SessionProvider({ children, session }: { children: React.ReactNode; session: Session | null }) {
-  return <NextAuthSessionProvider session={session}>{children}</NextAuthSessionProvider>;
+export function SessionProvider({ children }: { children: React.ReactNode }) {
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
 }
